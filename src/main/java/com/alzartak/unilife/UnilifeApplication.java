fe4dc19849da81d7538ecf2875bcc02f37c794e9
KiminSon <kimin0331@naver.com>
2023-12-21 18:19:43 +0900

    initial commit

---
+++ src/main/java/com/alzartak/unilife/UnilifeApplication.java
@@ -0,0 +1,13 @@
+package com.alzartak.unilife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnilifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnilifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alzartak/unilife/UnilifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alzartak.unilife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnilifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
